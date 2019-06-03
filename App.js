4836523fe842ac5f1e3658ea9d831eb6ba402160
Sullivan Prellwitz <prellwsr8859@uwec.edu>
2019-06-03 14:44:50 -0500

    Created starter expo app

--- App.js
+++ App.js
@@ -1,69 +1,62 @@
-import React, { Component } from "react";
-import {
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight
-} from "react-native";
-import LaundryScreen from "./Laundry.js";
-import styles from "./StyleSheet";
-import SpectatorScreen from "./Spectator.js";
-import {
-  createStackNavigator,
-  createAppContainer
-} from "react-navigation";
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
 
-class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  static navigationOptions = {
-    title: "UWEC",
-    headerStyle: {},
-    headerTitleStyle: {
-      position: "absolute",
-      left: 0
-    }
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
   };
 
-  render = () =>{
-    const { navigate } = this.props.navigation;
-    return (
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
         <View style={styles.container}>
-          <View style={styles.laundryView}>
-            <Text>Laundry Notifier</Text>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('Laundry')}>
-              <Image
-                style={styles.laundryImage}
-                source={require('./assets/laundry.png')}/>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.newsAndCarText}>
-            <Text style={styles.newsText}> Spectator</Text>
-            <Text style={styles.BluGoldIdText}> BluGold ID</Text>
-          </View>
-          <View style={styles.newsAndCarView}>
-              <TouchableHighlight
-               onPress={() => this.props.navigation.navigate('Spectator')}> 
-             <Image
-               style={styles.cardImage}
-                source={require('./assets/news.jpg')}
-            />
-            </TouchableHighlight>
-          </View>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
         </View>
-    );
+      );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
-const HomePageNavigator = createStackNavigator({
-    Home: {screen: HomeScreen},
-    Laundry: { screen: LaundryScreen },
-    Spectator: { screen: SpectatorScreen }
-  });
-
-  const App = createAppContainer(HomePageNavigator);
-  export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
