e40eeccb8ed2ff3114df40f54f86a140a8867d2e
Sullivan Prellwitz <prellwsr8859@uwec.edu>
2019-06-05 16:26:21 -0500

    Axios API hook up added

--- Laundry.js
+++ Laundry.js
@@ -8,79 +8,85 @@ import {
   Picker
 } from 'react-native';
 const axios = require('axios');
+import { LaundryQuery } from './LaundryQuery.js';
 
 class LaundryScreen extends Component {
   static navigationOptions = {
     title: 'Laundry Notifier'
   };
-  state = { laundryRoom: '' };
+  state = {
+    laundryRoom: '',
+    numDryers: '',
+    numWashers: '',
+    numDryersAvailable: '',
+    numWashersAvailable: ''
+  };
 
-  // const getLaundryRooms = async () => {
-  //   try {
-  //     return await axios.get('IP HERE');
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-  // const laundryRoom = async () => {
-  //   const selectedRoom = await getLaundryRooms();
-  //   return selectedRoom;
-  // };
-
-  updateLanguage = language => {
-    this.setState({ laundryRoom: language });
+  updateLaundry = laundry => {
     //this.laundryRoomSelection(this.state.laundryRoom);
+    axios({
+      url: 'http://35.247.62.2:4000/graphql',
+      method: 'post',
+      data: {
+        query: `
+                        {
+                            laundryRoom(building: "${laundry}") {
+                                totalNumDryers
+                                totalNumWashers 
+                                dryersAvailable
+                                washersAvailable
+                            }
+                        }
+                        `
+      }
+    }).then(result => {
+      this.setState({
+        laundryRoom: laundry,
+        numDryers: result.data.data.laundryRoom.totalNumDryers,
+        numWashers: result.data.data.laundryRoom.totalNumWashers,
+        numDryersAvailable: result.data.data.laundryRoom.dryersAvailable,
+        numWashersAvailable: result.data.data.laundryRoom.washersAvailable
+      });
+    });
   };
 
   render() {
-    const laundryRoom = () => {
-      return axios({
-        url: 'http://localhost:4000/graphql',
-        method: 'post',
-        data: {
-          query: `
-                  {
-                      laundryRoom(building: "Towers") {
-                          totalNumDryers
-                          totalNumWashers 
-                      }
-                  }
-                  `
-        }
-      }).then(result => {
-        return result.data;
-      });
-    };
-
-    let laundryRoomSelected = async () => {
-      return await laundryRoom();
-    };
-    let data = laundryRoomSelected();
-
     //const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text>{data}</Text>
         <Text>Testing text</Text>
         <Picker
           selectedValue={this.state.laundryRoom}
           style={{ height: 50, width: 100 }}
           onValueChange={(itemValue, itemIndex) =>
-            this.setState({ laundryRoom: itemValue })
+            this.updateLaundry(itemValue)
           }
         >
           <Picker.Item label="Bridgman" value="bridgman" />
           <Picker.Item label="Chancellors" value="chancellors" />
           <Picker.Item label="Governors" value="governors" />
           <Picker.Item label="Horan" value="horan" />
-          <Picker.Item label="Karlgaard Towers" value="Towers" />
+          <Picker.Item label="Karlgaard Towers" value="towers" />
           <Picker.Item label="Katharine Thomas" value="kt" />
           <Picker.Item label="Murray" value="murray" />
-          <Picker.Item label="Oak Ridge" value="oak ridge" />
+          <Picker.Item label="Oak Ridge" value="oak" />
           <Picker.Item label="Putnam" value="putnam" />
-          <Picker.Item label="The Suites" value="the suites" />
+          <Picker.Item label="The Suites" value="suites" />
           <Picker.Item label="Sutherland" value="sutherland" />
         </Picker>
+        <View style={styles.containerLaundryText}>
+          <Text>Laundry Room Selected: {this.state.laundryRoom}</Text>
+          <Text>
+            Number of Washers Available: {this.state.numWashersAvailable}
+          </Text>
+          <Text>
+            Number of Dryers Available: {this.state.numDryersAvailable}
+          </Text>
+          <Text>
+            Total Washers: {this.state.numWashers} | Total Dryers:{' '}
+            {this.state.numDryers}
+          </Text>
+        </View>
       </View>
     );
   }
