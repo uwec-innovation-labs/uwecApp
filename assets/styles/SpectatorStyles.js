f49da2924f4a7fe9a41c9da08bb29cb5d66f893e
Sullivan Prellwitz <prellwsr8859@uwec.edu>
2019-06-10 16:55:42 -0500

    Added Spectator articles!!

--- assets/styles/SpectatorStyles.js
+++ assets/styles/SpectatorStyles.js
@@ -1,7 +1,44 @@
 import { StyleSheet } from 'react-native'
+import { Dimensions } from 'react-native'
 
 export default StyleSheet.create({
   container: {
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 8
+  },
+  titleText: {
+    fontSize: 24
+  },
+  newsTitleText: {
+    fontSize: 20,
+    color: 'blue'
+  },
+  horizontalLine: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    marginLeft: 50,
+    marginRight: 50,
+    marginTop: 8,
+    marginBottom: 10
+  },
+  horizontalLineThin: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.5,
+    marginLeft: 60,
+    marginRight: 60,
+    marginTop: 6,
+    marginBottom: 6
+  },
+  newsStoryContainer: {
+    margin: 2,
+    padding: 2,
+    backgroundColor: 'rgba(122, 122, 122, 0.11)',
+    alignItems: 'center',
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 2
+  },
+  newsImage: {
+    width: 300,
+    height: 350
   }
 })
--- screens/Spectator.js
+++ screens/Spectator.js
@@ -1,16 +1,125 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  Linking,
+  FlatList
+} from 'react-native'
 import styles from '../assets/styles/SpectatorStyles.js'
+import { ScrollView } from 'react-native-gesture-handler'
+const axios = require('axios')
 
 class SpectatorScreen extends Component {
   static navigationOptions = {
     title: 'Spectator'
   }
+  state = {
+    titleArray: [],
+    imageLinkArray: [],
+    newsLinkArray: []
+  }
+  news = () => {
+    axios({
+      url: 'http://35.247.62.2:4000/graphql',
+      method: 'post',
+      data: {
+        query: `
+        {
+          news {
+            title
+            image
+            link
+          }
+        }
+        `
+      }
+    }).then(result => {
+      tempArr1 = []
+      tempArr2 = []
+      tempArr3 = []
+      for (i = 0; i < 5; i++) {
+        tempArr1.push(result.data.data.news[i].title)
+        tempArr2.push(result.data.data.news[i].image)
+        tempArr3.push(result.data.data.news[i].link)
+      }
+      this.setState({
+        titleArray: tempArr1,
+        imageLinkArray: tempArr2,
+        newsLinkArray: tempArr3
+      })
+    })
+  }
   render() {
+    this.news()
     const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
-        <Text> EXTRA EXTRA READ ABOUT IT </Text>
+        <Text style={styles.titleText}>The latest from The Spectator</Text>
+
+        <ScrollView contentContainerStyle={styles.container}>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.state.newsLinkArray[0]}`)}
+            style={styles.newsStoryContainer}
+          >
+            <Text style={styles.newsTitleText}>{this.state.titleArray[0]}</Text>
+
+            <Image
+              style={styles.newsImage}
+              source={{ uri: `${this.state.imageLinkArray[0]}` }}
+              resizeMode={'contain'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.state.newsLinkArray[1]}`)}
+            style={styles.newsStoryContainer}
+          >
+            <Text style={styles.newsTitleText}>{this.state.titleArray[1]}</Text>
+
+            <Image
+              style={styles.newsImage}
+              source={{ uri: `${this.state.imageLinkArray[1]}` }}
+              resizeMode={'contain'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.state.newsLinkArray[2]}`)}
+            style={styles.newsStoryContainer}
+          >
+            <Text style={styles.newsTitleText}>{this.state.titleArray[2]}</Text>
+
+            <Image
+              style={styles.newsImage}
+              source={{ uri: `${this.state.imageLinkArray[2]}` }}
+              resizeMode={'contain'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.state.newsLinkArray[3]}`)}
+            style={styles.newsStoryContainer}
+          >
+            <Text style={styles.newsTitleText}>{this.state.titleArray[3]}</Text>
+
+            <Image
+              style={styles.newsImage}
+              source={{ uri: `${this.state.imageLinkArray[3]}` }}
+              resizeMode={'contain'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.state.newsLinkArray[4]}`)}
+            style={styles.newsStoryContainer}
+          >
+            <Text style={styles.newsTitleText}>{this.state.titleArray[4]}</Text>
+
+            <Image
+              style={styles.newsImage}
+              source={{ uri: `${this.state.imageLinkArray[4]}` }}
+              resizeMode={'contain'}
+            />
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     )
   }
